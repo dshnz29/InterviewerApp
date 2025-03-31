05f444a127f0bd3ae6ca792e5de9e0cc1ef54f96
H.M.D.B. Bandara <dshnz29@gmail.com>
2025-04-01 01:57:58 +0530

    //

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,13 +1,15 @@
-import Agent from '@/components/Agent'
-import React from 'react'
+import Agent from "@/components/Agent";
+import {getCurrentUser} from "@/lib/actions/auth.action";
 
-const page = () => {
-  return (
-    <div>In
-        <Agent username ="You" userId="user1" type="generation"/>
-    </div>
-    
-  )
+const Page = async () => {
+    const user = await getCurrentUser();
+
+    return (
+        <>
+            <h3>Interview Generation</h3>
+
+            <Agent userName={user?.name} userId={user?.id} type="generate" />
+        </>
+    )
 }
-
-export default page
+export default Page
